46d9144eceb703078d95d0999515a7a00d2c77d7
Shane Gough <shane@thegaragelab.com>
2014-12-27 13:59:06 +1000

    Added hardware initialisation and some initial debug code to verify serial IO.

--- firmware/main.c
+++ firmware/main.c
@@ -5,22 +5,65 @@
 *
 * Template program for ATtiny85 C/asm projects.
 *--------------------------------------------------------------------------*/
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <microboard.h>
 #include <softuart.h>
+#include <softspi.h>
 #include <mbstring.h>
 
 // Program banner
 #define BANNER PSTR("EEPROG V0.1\n")
 
+// Pin definitions
+enum _PINDEF {
+  // SPI pins
+  MISO = 10,
+  MOSI = 13,
+  SCK  = 12,
+  CS   = 11,
+  // Power control
+  PWR_SPI = 6,
+  PWR_I2C = 5,
+  };
+
 // Forward definition of 'main()'
 void main() __attribute__ ((noreturn));
+
+/** SPI configuration
+ */
+static SSPI_INTERFACE s_SPI = {
+  { NULL },
+  SPI_PHASE_TRAILING | SPI_POLARITY_LOW | SPI_MSB_FIRST,
+  MISO,
+  MOSI,
+  SCK
+  };
+
+/** Initialise the hardware
+ */
+static void hwInit() {
+  // Initialise GPIO
+  pinConfig(CS, OUTPUT);
+  pinWrite(CS, true);
+  pinConfig(PWR_SPI, OUTPUT);
+  pinWrite(PWR_SPI, false);
+  pinConfig(PWR_I2C, OUTPUT);
+  pinWrite(PWR_I2C, false);
+  // Initialise subsystems
+  uartInit();
+  sspiInit(&s_SPI);
+  }
 
 /** Program entry point
  */
 void main()  {
-  uartInit();
+  hwInit();
   uartPrintP(BANNER);
-  while(true);
+  while(true) {
+    // DEBUG: For now, just echo back to make sure the code is running
+    uint8_t ch = uartRead();
+    uartWrite(ch);
+    }
   }
