1e3e7ac0e45b456b8cdfba97890e9679969dc40f
Shane Gough <shane@thegaragelab.com>
2014-12-30 14:55:07 +1000

    Started working on Windows client application for the programmer.

---
+++ software/eeprog/MainForm.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace eeprog
+{
+  partial class MainForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.m_lstChipType = new System.Windows.Forms.ComboBox();
+      this.label3 = new System.Windows.Forms.Label();
+      this.m_lstAddressSize = new System.Windows.Forms.ComboBox();
+      this.label6 = new System.Windows.Forms.Label();
+      this.m_lstCapacity = new System.Windows.Forms.ComboBox();
+      this.label5 = new System.Windows.Forms.Label();
+      this.m_lstPageSize = new System.Windows.Forms.ComboBox();
+      this.label4 = new System.Windows.Forms.Label();
+      this.groupBox2 = new System.Windows.Forms.GroupBox();
+      this.m_progress = new System.Windows.Forms.ProgressBar();
+      this.m_btnWrite = new System.Windows.Forms.Button();
+      this.m_btnRead = new System.Windows.Forms.Button();
+      this.m_lstEEPROM = new System.Windows.Forms.ComboBox();
+      this.label2 = new System.Windows.Forms.Label();
+      this.m_lstPort = new System.Windows.Forms.ComboBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.tabControl1 = new System.Windows.Forms.TabControl();
+      this.tabMessages = new System.Windows.Forms.TabPage();
+      this.m_txtMessages = new System.Windows.Forms.TextBox();
+      this.tabComms = new System.Windows.Forms.TabPage();
+      this.m_txtComms = new System.Windows.Forms.TextBox();
+      this.groupBox1.SuspendLayout();
+      this.groupBox2.SuspendLayout();
+      this.tabControl1.SuspendLayout();
+      this.tabMessages.SuspendLayout();
+      this.tabComms.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.m_lstChipType);
+      this.groupBox1.Controls.Add(this.label3);
+      this.groupBox1.Controls.Add(this.m_lstAddressSize);
+      this.groupBox1.Controls.Add(this.label6);
+      this.groupBox1.Controls.Add(this.m_lstCapacity);
+      this.groupBox1.Controls.Add(this.label5);
+      this.groupBox1.Controls.Add(this.m_lstPageSize);
+      this.groupBox1.Controls.Add(this.label4);
+      this.groupBox1.Location = new System.Drawing.Point(12, 12);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(216, 134);
+      this.groupBox1.TabIndex = 6;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "EEPROM Info";
+      // 
+      // m_lstChipType
+      // 
+      this.m_lstChipType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.m_lstChipType.Enabled = false;
+      this.m_lstChipType.FormattingEnabled = true;
+      this.m_lstChipType.Location = new System.Drawing.Point(84, 19);
+      this.m_lstChipType.Name = "m_lstChipType";
+      this.m_lstChipType.Size = new System.Drawing.Size(121, 21);
+      this.m_lstChipType.TabIndex = 11;
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(10, 22);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(31, 13);
+      this.label3.TabIndex = 10;
+      this.label3.Text = "Type";
+      // 
+      // m_lstAddressSize
+      // 
+      this.m_lstAddressSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.m_lstAddressSize.Enabled = false;
+      this.m_lstAddressSize.FormattingEnabled = true;
+      this.m_lstAddressSize.Location = new System.Drawing.Point(84, 100);
+      this.m_lstAddressSize.Name = "m_lstAddressSize";
+      this.m_lstAddressSize.Size = new System.Drawing.Size(121, 21);
+      this.m_lstAddressSize.TabIndex = 9;
+      // 
+      // label6
+      // 
+      this.label6.AutoSize = true;
+      this.label6.Location = new System.Drawing.Point(10, 103);
+      this.label6.Name = "label6";
+      this.label6.Size = new System.Drawing.Size(68, 13);
+      this.label6.TabIndex = 8;
+      this.label6.Text = "Address Size";
+      // 
+      // m_lstCapacity
+      // 
+      this.m_lstCapacity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.m_lstCapacity.Enabled = false;
+      this.m_lstCapacity.FormattingEnabled = true;
+      this.m_lstCapacity.Location = new System.Drawing.Point(84, 73);
+      this.m_lstCapacity.Name = "m_lstCapacity";
+      this.m_lstCapacity.Size = new System.Drawing.Size(121, 21);
+      this.m_lstCapacity.TabIndex = 7;
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(10, 76);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(48, 13);
+      this.label5.TabIndex = 6;
+      this.label5.Text = "Capacity";
+      // 
+      // m_lstPageSize
+      // 
+      this.m_lstPageSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.m_lstPageSize.Enabled = false;
+      this.m_lstPageSize.FormattingEnabled = true;
+      this.m_lstPageSize.Location = new System.Drawing.Point(84, 46);
+      this.m_lstPageSize.Name = "m_lstPageSize";
+      this.m_lstPageSize.Size = new System.Drawing.Size(121, 21);
+      this.m_lstPageSize.TabIndex = 5;
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(10, 49);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(55, 13);
+      this.label4.TabIndex = 4;
+      this.label4.Text = "Page Size";
+      // 
+      // groupBox2
+      // 
+      this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.groupBox2.Controls.Add(this.m_progress);
+      this.groupBox2.Controls.Add(this.m_btnWrite);
+      this.groupBox2.Controls.Add(this.m_btnRead);
+      this.groupBox2.Controls.Add(this.m_lstEEPROM);
+      this.groupBox2.Controls.Add(this.label2);
+      this.groupBox2.Controls.Add(this.m_lstPort);
+      this.groupBox2.Controls.Add(this.label1);
+      this.groupBox2.Location = new System.Drawing.Point(234, 13);
+      this.groupBox2.Name = "groupBox2";
+      this.groupBox2.Size = new System.Drawing.Size(266, 133);
+      this.groupBox2.TabIndex = 8;
+      this.groupBox2.TabStop = false;
+      this.groupBox2.Text = "Activity";
+      // 
+      // m_progress
+      // 
+      this.m_progress.Location = new System.Drawing.Point(17, 104);
+      this.m_progress.Name = "m_progress";
+      this.m_progress.Size = new System.Drawing.Size(239, 23);
+      this.m_progress.TabIndex = 15;
+      // 
+      // m_btnWrite
+      // 
+      this.m_btnWrite.Location = new System.Drawing.Point(181, 72);
+      this.m_btnWrite.Name = "m_btnWrite";
+      this.m_btnWrite.Size = new System.Drawing.Size(75, 23);
+      this.m_btnWrite.TabIndex = 13;
+      this.m_btnWrite.Text = "Write";
+      this.m_btnWrite.UseVisualStyleBackColor = true;
+      this.m_btnWrite.Click += new System.EventHandler(this.OnWriteClick);
+      // 
+      // m_btnRead
+      // 
+      this.m_btnRead.Location = new System.Drawing.Point(97, 72);
+      this.m_btnRead.Name = "m_btnRead";
+      this.m_btnRead.Size = new System.Drawing.Size(75, 23);
+      this.m_btnRead.TabIndex = 12;
+      this.m_btnRead.Text = "Read";
+      this.m_btnRead.UseVisualStyleBackColor = true;
+      this.m_btnRead.Click += new System.EventHandler(this.OnReadClick);
+      // 
+      // m_lstEEPROM
+      // 
+      this.m_lstEEPROM.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.m_lstEEPROM.FormattingEnabled = true;
+      this.m_lstEEPROM.Location = new System.Drawing.Point(100, 45);
+      this.m_lstEEPROM.Name = "m_lstEEPROM";
+      this.m_lstEEPROM.Size = new System.Drawing.Size(156, 21);
+      this.m_lstEEPROM.TabIndex = 11;
+      this.m_lstEEPROM.SelectedIndexChanged += new System.EventHandler(this.OnEEPROMChanged);
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(14, 48);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(80, 13);
+      this.label2.TabIndex = 10;
+      this.label2.Text = "EEPROM Type";
+      // 
+      // m_lstPort
+      // 
+      this.m_lstPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.m_lstPort.FormattingEnabled = true;
+      this.m_lstPort.Location = new System.Drawing.Point(100, 18);
+      this.m_lstPort.Name = "m_lstPort";
+      this.m_lstPort.Size = new System.Drawing.Size(156, 21);
+      this.m_lstPort.TabIndex = 9;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(14, 21);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(53, 13);
+      this.label1.TabIndex = 8;
+      this.label1.Text = "COM Port";
+      // 
+      // tabControl1
+      // 
+      this.tabControl1.Controls.Add(this.tabMessages);
+      this.tabControl1.Controls.Add(this.tabComms);
+      this.tabControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.tabControl1.Location = new System.Drawing.Point(0, 152);
+      this.tabControl1.Name = "tabControl1";
+      this.tabControl1.SelectedIndex = 0;
+      this.tabControl1.Size = new System.Drawing.Size(509, 183);
+      this.tabControl1.TabIndex = 9;
+      // 
+      // tabMessages
+      // 
+      this.tabMessages.Controls.Add(this.m_txtMessages);
+      this.tabMessages.Location = new System.Drawing.Point(4, 22);
+      this.tabMessages.Name = "tabMessages";
+      this.tabMessages.Padding = new System.Windows.Forms.Padding(3);
+      this.tabMessages.Size = new System.Drawing.Size(501, 157);
+      this.tabMessages.TabIndex = 0;
+      this.tabMessages.Text = "Messages";
+      this.tabMessages.UseVisualStyleBackColor = true;
+      // 
+      // m_txtMessages
+      // 
+      this.m_txtMessages.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.m_txtMessages.Location = new System.Drawing.Point(3, 3);
+      this.m_txtMessages.Multiline = true;
+      this.m_txtMessages.Name = "m_txtMessages";
+      this.m_txtMessages.ReadOnly = true;
+      this.m_txtMessages.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+      this.m_txtMessages.Size = new System.Drawing.Size(495, 151);
+      this.m_txtMessages.TabIndex = 0;
+      // 
+      // tabComms
+      // 
+      this.tabComms.Controls.Add(this.m_txtComms);
+      this.tabComms.Location = new System.Drawing.Point(4, 22);
+      this.tabComms.Name = "tabComms";
+      this.tabComms.Padding = new System.Windows.Forms.Padding(3);
+      this.tabComms.Size = new System.Drawing.Size(501, 157);
+      this.tabComms.TabIndex = 1;
+      this.tabComms.Text = "Communications";
+      this.tabComms.UseVisualStyleBackColor = true;
+      // 
+      // m_txtComms
+      // 
+      this.m_txtComms.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.m_txtComms.Location = new System.Drawing.Point(3, 3);
+      this.m_txtComms.Multiline = true;
+      this.m_txtComms.Name = "m_txtComms";
+      this.m_txtComms.ReadOnly = true;
+      this.m_txtComms.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+      this.m_txtComms.Size = new System.Drawing.Size(495, 151);
+      this.m_txtComms.TabIndex = 1;
+      // 
+      // MainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(509, 335);
+      this.Controls.Add(this.tabControl1);
+      this.Controls.Add(this.groupBox2);
+      this.Controls.Add(this.groupBox1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+      this.MaximizeBox = false;
+      this.Name = "MainForm";
+      this.Text = "EEProg - The Garage Lab";
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox1.PerformLayout();
+      this.groupBox2.ResumeLayout(false);
+      this.groupBox2.PerformLayout();
+      this.tabControl1.ResumeLayout(false);
+      this.tabMessages.ResumeLayout(false);
+      this.tabMessages.PerformLayout();
+      this.tabComms.ResumeLayout(false);
+      this.tabComms.PerformLayout();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.GroupBox groupBox1;
+    private System.Windows.Forms.ComboBox m_lstAddressSize;
+    private System.Windows.Forms.Label label6;
+    private System.Windows.Forms.ComboBox m_lstCapacity;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.ComboBox m_lstPageSize;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.ComboBox m_lstChipType;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.GroupBox groupBox2;
+    private System.Windows.Forms.ProgressBar m_progress;
+    private System.Windows.Forms.Button m_btnWrite;
+    private System.Windows.Forms.Button m_btnRead;
+    private System.Windows.Forms.ComboBox m_lstEEPROM;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.ComboBox m_lstPort;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TabControl tabControl1;
+    private System.Windows.Forms.TabPage tabMessages;
+    private System.Windows.Forms.TextBox m_txtMessages;
+    private System.Windows.Forms.TabPage tabComms;
+    private System.Windows.Forms.TextBox m_txtComms;
+  }
+}
+
---
+++ software/eeprog/MainForm.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eeprog
+{
+  public partial class MainForm : Form
+  {
+    private Dictionary<string, EEPROM> m_eeproms;
+    private DeviceLoader m_loader;
+
+    public MainForm()
+    {
+      InitializeComponent();
+      m_loader = new DeviceLoader();
+      m_loader.ConnectionStateChanged += OnConnectionStateChanged;
+      m_loader.Error += OnError;
+      m_loader.Communications += OnCommunications;
+      m_loader.Progress += OnProgress;
+      // Populate known EEPROM types
+      m_eeproms = new Dictionary<string, EEPROM>();
+      m_eeproms.Add(
+        "25AA640A",
+        new EEPROM(ConnectionType.SPI, 5, 13, 2)
+        );
+      m_eeproms.Add(
+        "25AA1024",
+        new EEPROM(ConnectionType.SPI, 8, 17, 3)
+        );
+      // Populate lists
+      m_lstChipType.Items.Add(ConnectionType.I2C.ToString());
+      m_lstChipType.Items.Add(ConnectionType.SPI.ToString());
+      for (int i = 0; i <= 8; i++)
+        m_lstPageSize.Items.Add((1 << i).ToString());
+      for (int i = 0; i <= 7; i++)
+        m_lstCapacity.Items.Add((1 << i).ToString() + "K");
+      for (int i = 1; i <= 4; i++)
+        m_lstAddressSize.Items.Add(i.ToString());
+      // Populate the list of available COM ports
+      List<string> ports = new List<string>(SerialPort.GetPortNames());
+      ports.Sort();
+      m_lstPort.Items.AddRange(ports.ToArray());
+      if (m_lstPort.Items.Count > 0)
+        m_lstPort.SelectedIndex = 0;
+      // Do EEPROM last, it will trigger changes in other fields
+      foreach (string name in m_eeproms.Keys)
+        m_lstEEPROM.Items.Add(name);
+      m_lstEEPROM.SelectedIndex = 0;
+    }
+
+    #region "UI Events"
+
+    private void OnEEPROMChanged(object sender, EventArgs e)
+    {
+      EEPROM eeprom;
+      if (!m_eeproms.TryGetValue(m_lstEEPROM.SelectedItem.ToString(), out eeprom))
+        return; // TODO: Should not happen
+      // Update the EEPROM info field
+      m_lstChipType.SelectedItem = eeprom.Connection.ToString();
+      m_lstPageSize.SelectedItem = eeprom.PageSize.ToString();
+      m_lstCapacity.SelectedItem = eeprom.SizeInK;
+      m_lstAddressSize.SelectedItem = eeprom.AddressBytes.ToString();
+    }
+
+    private void OnReadClick(object sender, EventArgs e)
+    {
+      // Get the port and the EEPROM to use
+      string port = m_lstPort.SelectedItem.ToString();
+      EEPROM eeprom;
+      if (!m_eeproms.TryGetValue(m_lstEEPROM.SelectedItem.ToString(), out eeprom))
+        {
+        MessageBox.Show("No EEPROM selected.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        return;
+        }
+      // Determine where to save the file
+      SaveFileDialog dlg = new SaveFileDialog();
+      dlg.DefaultExt = "rom";
+      dlg.AddExtension = true;
+      dlg.OverwritePrompt = true;
+      dlg.Title = "Save ROM Image";
+      dlg.Filter = "ROM Image (*.rom)|*.rom";
+      DialogResult result = dlg.ShowDialog();
+      if (result == DialogResult.OK)
+      {
+        // Start the reading task and save the data if successful
+        Task.Factory.StartNew(() => {
+          m_loader.Read(port, eeprom, 0, eeprom.Size, new FileInfo(dlg.FileName));
+          });
+      }
+    }
+
+    private void OnWriteClick(object sender, EventArgs e)
+    {
+      // Get the port and the EEPROM to use
+      string port = m_lstPort.SelectedItem.ToString();
+      EEPROM eeprom;
+      if (!m_eeproms.TryGetValue(m_lstEEPROM.SelectedItem.ToString(), out eeprom))
+      {
+        MessageBox.Show("No EEPROM selected.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        return;
+      }
+      // Determine what file to write
+      OpenFileDialog dlg = new OpenFileDialog();
+      dlg.DefaultExt = "rom";
+      dlg.AddExtension = true;
+      dlg.CheckFileExists = true;
+      dlg.Title = "Open ROM Image";
+      dlg.Filter = "ROM Image (*.rom)|*.rom";
+      DialogResult result = dlg.ShowDialog();
+      if (result == DialogResult.OK)
+      {
+        // Start the reading task and save the data if successful
+        Task.Factory.StartNew(() =>
+        {
+          m_loader.Write(port, eeprom, 0, new FileInfo(dlg.FileName));
+        });
+      }
+    }
+
+    #endregion
+
+    private void LogMessage(TextBox target, string message)
+    {
+      target.AppendText(message + "\n");
+    }
+
+    private void UpdateUI()
+    {
+      if (m_loader.Operation == Operation.Idle)
+      {
+        m_btnRead.Enabled = true;
+        m_btnWrite.Enabled = true;
+        m_lstEEPROM.Enabled = true;
+        m_lstPort.Enabled = true;
+        m_progress.Value = 0;
+      }
+      else
+      {
+        m_btnRead.Enabled = false;
+        m_btnWrite.Enabled = false;
+        m_lstEEPROM.Enabled = false;
+        m_lstPort.Enabled = false;
+      }
+    }
+
+    #region "Device Events"
+
+    void OnProgress(DeviceLoader sender, ProgressState state, int position, int target, string message)
+    {
+      if (InvokeRequired)
+      {
+        BeginInvoke(new Action(() => { OnProgress(sender, state, position, target, message); }));
+        return;
+      }
+      // Safe to use UI
+      m_progress.Maximum = target;
+      m_progress.Value = position;
+      if (message != null)
+        LogMessage(m_txtMessages, message);
+    }
+
+    void OnCommunications(DeviceLoader sender, Direction direction, string content)
+    {
+      if (InvokeRequired)
+      {
+        BeginInvoke(new Action(() => { OnCommunications(sender, direction, content); }));
+        return;
+      }
+      // Safe to use UI
+      LogMessage(m_txtComms, String.Format("{0} {1}", (direction == Direction.Input) ? "<" : ">", content));
+    }
+
+    void OnError(DeviceLoader sender, string message, Exception ex)
+    {
+      if (InvokeRequired)
+      {
+        BeginInvoke(new Action(() => { OnError(sender, message, ex); }));
+        return;
+      }
+      // Safe to use UI
+      if (ex != null)
+        message = message + "\n" + ex.Message;
+      LogMessage(m_txtMessages, "ERROR: " + message + "\n");
+      MessageBox.Show(message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+    }
+
+    void OnConnectionStateChanged(DeviceLoader sender, ConnectionState state)
+    {
+      if (InvokeRequired)
+      {
+        BeginInvoke(new Action(() => { OnConnectionStateChanged(sender, state); }));
+        return;
+      }
+      // Safe to use UI
+      UpdateUI();
+      LogMessage(m_txtMessages, String.Format("Programmer {0}", state));
+    }
+
+    #endregion
+
+  }
+}
